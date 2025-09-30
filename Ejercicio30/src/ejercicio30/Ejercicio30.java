db356a08a1f695b20c4f9ae78a24517d229bbbef
vjp-gabrielRM <gruzm01@informatica.iesvalledeljerteplasencia.es>
2025-09-30 17:39:19 +0200

    ejercicio 30

---
+++ Ejercicio30/src/ejercicio30/Ejercicio30.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio30;
+
+    import java.util.Scanner;
+
+public class Ejercicio30 {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    char letra = 'A';
+    
+    int codigo = letra;
+    
+    System.out.println("El codigo ASCII de " + letra + " es " + codigo + ".");
+    
+    letra = 'B';
+    
+    codigo = letra;
+    
+    System.out.println("El codigo ASCII de " + letra + " es " + codigo + ".");
+    
+    letra = 'C';
+    
+    codigo = letra;
+    
+    System.out.println("El codigo ASCII de " + letra + " es " + codigo + ".");
+    
+    letra = 'a';
+    
+    codigo = letra;
+    
+    System.out.println("El codigo ASCII de " + letra + " es " + codigo + ".");
+    
+    letra = 'b';
+    
+    codigo = letra;
+    
+    System.out.println("El codigo ASCII de " + letra + " es " + codigo + ".");
+    
+    letra = 'c';
+    
+    codigo = letra;
+    
+    System.out.println("El codigo ASCII de " + letra + " es " + codigo + ".");
+    
+    letra = '0';
+    
+    codigo = letra;
+    
+    System.out.println("El codigo ASCII de " + letra + " es " + codigo + ".");
+    
+    letra = '1';
+    
+    codigo = letra;
+    
+    System.out.println("El codigo ASCII de " + letra + " es " + codigo + ".");
+    
+    letra = '2';
+    
+    codigo = letra;
+    
+    System.out.println("El codigo ASCII de " + letra + " es " + codigo + ".");
+    
+    letra = '$';
+    
+    codigo = letra;
+    
+    System.out.println("El codigo ASCII de " + letra + " es " + codigo + ".");
+    
+    letra = '*';
+    
+    codigo = letra;
+    
+    System.out.println("El codigo ASCII de " + letra + " es " + codigo + ".");
+    
+    letra = '+';
+    
+    codigo = letra;
+    
+    System.out.println("El codigo ASCII de " + letra + " es " + codigo + ".");
+    
+    letra = '/';
+    
+    codigo = letra;
+    
+    System.out.println("El codigo ASCII de " + letra + " es " + codigo + ".");
+    
+    letra = ' ';
+    
+    codigo = letra;
+    
+    System.out.println("El codigo ASCII de " + letra + " es " + codigo + ".");
+    
+        // TODO code application logic here
+    }
+    
+}
