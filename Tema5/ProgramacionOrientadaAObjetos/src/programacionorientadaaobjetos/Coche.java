19c83d9614f4e748e5e2fa254b100bab36fd3340
vjp-gabrielRM <gruzm01@informatica.iesvalledeljerteplasencia.es>
2025-11-07 20:17:27 +0100

    POO clase 7 noviembre

---
+++ Tema5/ProgramacionOrientadaAObjetos/src/programacionorientadaaobjetos/Coche.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programacionorientadaaobjetos;
+
+/**
+ *
+ * @author alumno
+ */
+public class Coche {
+    //Atributos, no se inicializan
+    private String ruedas;
+    private String suspension;
+    private int espejos;
+    
+    //Constructores
+    //Constructor por defecto
+    public Coche(){
+        this.ruedas = "";
+        this.suspension = "";
+        this.espejos = 0;
+    }
+    
+    //Constructor parametrizado
+    public Coche(String ruedas, String suspension, int espejos){
+        this.ruedas = ruedas;
+        this.suspension = suspension;
+        this.espejos = espejos;
+    }
+    
+    //Getters y setters
+    public String getRuedas(){
+        return this.ruedas;
+    }
+    
+    public void setRuedas(String ruedas){
+        this.ruedas = ruedas;
+    }
+    
+    public String getSuspension(){
+        return this.suspension;
+    }
+    
+    public void setSuspension(String suspension){
+        this.suspension = suspension;
+    }
+    
+    public int getEspejos(){
+        return this.espejos;
+    }
+    
+    public void setEspejos(int espejos){
+        this.espejos = espejos;
+    }
+    
+    //Otros métodos
+    public void acelerar(){
+        System.out.println("Acelerando");
+    }
+    
+    public void frenar(){
+        System.out.println("Frenando");
+    }
+    
+    public void girar(){
+        System.out.println("Girando");
+    }
+    
+    //To String
+    @Override
+    public String toString(){
+        return "Mi coche tiene ruedas " + this.ruedas + " suspensión " + this.suspension + " y " + this.espejos + " espejos";
+    }
+}
---
+++ Tema5/ProgramacionOrientadaAObjetos/src/programacionorientadaaobjetos/ProgramacionOrientadaAObjetos.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programacionorientadaaobjetos;
+
+/**
+ *
+ * @author alumno
+ */
+public class ProgramacionOrientadaAObjetos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //    Coche miCoche = new Coche();
+    //    System.out.println(miCoche.toString());
+        
+    //    miCoche.setEspejos(5);
+    //    System.out.println(miCoche.toString());
+        
+    //    System.out.println(miCoche.getEspejos());
+        
+    //    miCoche.acelerar();
+        //gabriel es un degenerado
+        Coche tuCoche = new Coche("Michelin", "Baja", 3);
+        System.out.println(tuCoche.toString());
+        System.out.println(tuCoche);
+        
+        System.out.println(tuCoche.getRuedas());
+        // TODO code application logic here
+        System.out.println("Gabriel es un degenerado en busca de niñas");
+    }
+    
+}
