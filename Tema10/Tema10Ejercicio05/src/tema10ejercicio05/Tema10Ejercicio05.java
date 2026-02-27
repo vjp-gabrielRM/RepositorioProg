3996a0c297699b58f07eeb7f79fb9d602389564f
vjp-gabrielRM <gruzm01@informatica.iesvalledeljerteplasencia.es>
2026-02-27 20:15:41 +0100

    Clase 27 de febrero

---
+++ Tema10/Tema10Ejercicio05/src/tema10ejercicio05/Tema10Ejercicio05.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tema10ejercicio05;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class Tema10Ejercicio05 {
+
+    public static void rellenarLista(ArrayList<Integer> lista){
+        Scanner entrada = new Scanner(System.in);
+        boolean seguir = true;
+        int num;
+        do{
+            System.out.print("Introduzca un número positivo, si quiere terminar uno negativo: ");
+            num = entrada.nextInt();
+            if(num >= 0){
+                lista.add(num);
+            }else{
+                System.out.println("Saliendo...");
+                seguir = false;
+            }
+            entrada.nextLine();
+        }while(seguir);
+    }
+    
+    public static void mostrarLista(ArrayList<Integer> lista){
+        for(int i = 0; i < lista.size(); i++){
+            System.out.print(lista.get(i));
+            if(i < lista.size() - 1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println("");
+    }
+    
+    public static void intercambio(ArrayList<Integer> lista, int pos1, int pos2){
+        int aux;
+        aux = lista.get(pos1);
+        lista.set(pos1, lista.get(pos2));
+        lista.set(pos2, aux);
+        System.out.println("Intercambiando elementos " + (pos1 + 1) + " y " + (pos2 + 1) + "...");
+    }
+    
+    public static int mayorPar(ArrayList<Integer> lista) {
+        int resultado;
+        int pos = 0;
+        for (int i = 1; i < lista.size(); i++) {
+            if (lista.get(pos) < lista.get(i) && lista.get(pos) % 2 = 0) {
+                pos = i;
+            }
+        }
+        return resultado;
+    }
+    
+    public static int menorImpar(ArrayList<Integer> lista) {
+        int resultado;     
+        int pos = 0;
+        for (int i = 1; i < lista.size(); i++) {
+            if (lista.get(pos) > lista.get(i) && lista.get(pos) % 2 = 1) {
+                pos = i;
+            }
+        }
+        
+        return resultado;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        rellenarLista(lista);
+        mostrarLista(lista);
+        intercambio(lista, mayorPar(lista), menorImpar(lista));
+        
+    }
+    
+}
